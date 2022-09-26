893e43de16788c172e265859ce2f180c50a2976c
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-09-26 19:17:17 -0300

    Content Conteiner component

---
+++ src/Components/MainContent/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css'
+
+function MainContent() {
+  return (
+    <div className={ 'content' }>
+      Conteudo
+    </div>
+  )
+}
+
+export default MainContent;
