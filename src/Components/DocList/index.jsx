35804ccd8e4fabe5513737bbc7bcf2135caf9189
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-09-27 19:51:35 -0300

    add doc mock

--- src/Components/DocList/index.jsx
+++ src/Components/DocList/index.jsx
@@ -1,17 +1,22 @@
 import React, { useContext } from 'react';
 import ResearchContext from '../../Context/ResearchContext';
 import DocCard from './DocCard';
+import mockDocList from '../../Mock/apiMock.json';
 
 function DocList() {
   
-  const { docList, loading } = useContext(ResearchContext)
+  const {
+    //docList,
+    loading,
+  } = useContext(ResearchContext)
 
   const renderDocList = () => {
     if (!loading) {
       return (
-        docList.map((doc, index) => {
+        mockDocList.map((doc, index) => {
+          console.log(doc.authors)
           return (
-            <DocCard key={index} document={ doc } />
+            <DocCard key={ index } document={ doc }/>
           )
         })
       )
