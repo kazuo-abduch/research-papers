3bbae2185f37b945c819145d4d8942d310946b85
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-09-27 19:18:38 -0300

    Fix useEffect

--- src/Components/MainContent/index.jsx
+++ src/Components/MainContent/index.jsx
@@ -1,26 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
+import ResearchContext from '../../Context/ResearchContext';
 import Filter from '../Filter';
-import TableList from '../TableList';
+import DocList from '../DocList';
 import { fetchDocuments } from '../../Services/api';
 import './style.css'
 
 function MainContent() {
 
-  const [docList, setDocList] = useState();
+  const { setDocList, setLoad } = useContext(ResearchContext);
 
   useEffect(() => {
     const requestDocuments = async () => {
       const requestResult = await fetchDocuments()
       setDocList(requestResult)
     }
+    setLoad(false);
     requestDocuments();
-    console.log(docList)
-  }, [docList])
+  }, [])
 
   return (
     <div className={ 'content' }>
       <Filter />
-      <TableList />
+      <DocList/>
     </div>
   )
 }
