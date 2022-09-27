742eb3b5f4312cdc75310e2d70e3f37516cb049d
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-09-27 19:18:19 -0300

    add doclist and loading to state

--- src/Context/ResearchProvider.jsx
+++ src/Context/ResearchProvider.jsx
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 import ResearchContext from './ResearchContext';
 
 function PostListProvider({ children }) {
+  const [docList, setDocList] = useState();
   const [favoriteList, setFavList] = useState();
+  const [loading, setLoad] = useState(true);
 
   return (
     <ResearchContext.Provider
       value={ {
+        docList,
+        setDocList,
         favoriteList,
         setFavList,
+        loading,
+        setLoad,
       } }
     >
       { children }
