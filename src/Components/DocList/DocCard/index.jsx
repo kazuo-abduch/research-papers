af81e6993bfc5e70fc9e904bbda30f567166d688
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-09-27 19:51:55 -0300

    render authors

--- src/Components/DocList/DocCard/index.jsx
+++ src/Components/DocList/DocCard/index.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
 function DocCard(props) {
+  
   const { authors, downloadUrl, documentType, title, abstract } = props.document
+  
+  const renderNames = () => {
+    return authors.map((author, index) => {
+      return (
+        <span key={ index }>
+          {author.name}
+        </span>
+      )
+    })
+  }
+
   return (
-    <div>
-      <div> { authors.name } </div>
+    <div className='doc-card'>
       <div> { documentType } </div>
       <div> { title } </div>
       <div> { abstract } </div>
+      <div> { renderNames() } </div>
       <div> { downloadUrl } </div>
     </div>
   )
