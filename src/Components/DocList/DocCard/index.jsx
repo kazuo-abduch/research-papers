b92802129118325770685ed0b55a4c7e7a5567bd
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-09-28 19:17:43 -0300

    set favorite icon

--- src/Components/DocList/DocCard/index.jsx
+++ src/Components/DocList/DocCard/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FavIconChecked from '../../../images/FavIconChecked.svg';
+import FavIconUnchecked from '../../../images/FavIconUnchecked.svg';
 import './style.css';
 
 function DocCard(props) {
   
   const { _type, _source: { authors, description, title, urls } } = props.document
+
+  const [isFavorite, setFavorite] = useState(false)
   
   const renderNames = () => {
     return authors.map((author, index) => {
@@ -26,25 +30,42 @@ function DocCard(props) {
     })
   }
 
+  const markAsFavorite = () => {
+    const toggleFavorite = {
+      true: false,
+      false: true,
+    }
+    setFavorite(toggleFavorite[isFavorite]);
+  }
+
   return (
     <div className='doc-card'>
       <div>
-        <span>Type: </span>
-        { _type }
+        <div>
+          <span>Type: </span>
+          { _type }
+        </div>
+        <div>
+          <span>Title: </span>
+          { title }
+        </div>
+        <div>
+          <div>Description: </div>
+          { description }
+        </div>
+        <div>
+          <div>Authors: </div>
+          { renderNames() }
+        </div>
+        { renderUrls() }
       </div>
-      <div>
-        <span>Title: </span>
-        { title }
-      </div>
-      <div>
-        <div>Description: </div>
-        { description }
-      </div>
-      <div>
-        <div>Authors: </div>
-        { renderNames() }
-      </div>
-      { renderUrls() }
+      <input
+        className='fav-button'
+        type="image"
+        src={ isFavorite ? FavIconChecked : FavIconUnchecked }
+        alt="fav button"
+        onClick={ markAsFavorite }
+      />
     </div>
   )
 }
