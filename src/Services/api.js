c75d505c3ab1215add62b0a6581ea971ff36dba3
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-09-27 22:34:42 -0300

    change api link

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,20 +1,11 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
-const url = 'https://api.core.ac.uk/v3/search/works';
-
-const h = new Headers();
-h.append('Authorization', `Bearer ${API_KEY}`);
-
-const request = new Request(url, {
-  method: 'GET',
-  headers: h,
-  mode: 'cors'
-});
+const url = 'https://core.ac.uk/api-v2/search/works?page=1&pageSize=10&';
 
 export const fetchDocuments = async () => {
   try {
-    const result = await fetch(request);
+    const result = await fetch(`${url}&apiKey=${API_KEY}`);
     const resultJSON = await result.json();
-    return resultJSON.results
+    return resultJSON.results;
   } catch (error) {
     console.log(error);
   }
